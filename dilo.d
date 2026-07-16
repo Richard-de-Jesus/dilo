e3408226dfa0c1d8fb8f1f2c80e334043b05ef9d
nerd184 <135471576+nerd184@users.noreply.github.com>
2026-07-16 10:25:56 -0300

    translated enableRawMode
    also cleaned the code to replace gotos with scope(exit)

--- dilo.d
+++ dilo.d
@@ -6,41 +6,94 @@ import std;
 // resolve conflicts between libc and core.stdc.
 // we prefer D's version, more type safety.
 alias libc = core.stdc;
-alias cio = libc.stdio; 
-
+alias cio = libc.stdio;
 /* making clear that kilo namespace is used
  to resolve conflicts between symbols built in C
   and D's reimport of those symbols in core.* */
 alias cbuiltin = kilo;
 
+// dmd is not able to handle the errno macro
+// so just pulled up the original name in
+// glibc. probably not portable.
+alias diloErrno = __errno_location;
+
+/* Raw mode: 1960 magic shit. */
+int enableRawMode(int fd)
+{
+    termios raw;
+    if (kilo.E.rawmode)
+        return 0; /* Already enabled. */
+
+    bool err = true;
+    // replaced 'goto fatal' with scope(exit)
+    scope (exit)
+        if (err) *diloErrno() = ENOTTY;
+    
+    if (!isatty(STDIN_FILENO))
+        return -1;
+
+    atexit(&editorAtExit);
+    if (tcgetattr(fd, &orig_termios) == -1)
+        return -1;
+
+    raw = orig_termios; /* modify the original mode */
+    /* input modes: no break, no CR to NL, no parity check, no strip char,
+     * no start/stop output control. */
+    raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+    /* output modes - disable post processing */
+    raw.c_oflag &= ~(OPOST);
+    /* control modes - set 8 bit chars */
+    raw.c_cflag |= (CS8);
+    /* local modes - choing off, canonical off, no extended functions,
+     * no signal chars (^Z,^C) */
+    raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+    /* control chars - set return condition: min number of bytes and timer. */
+    raw.c_cc[VMIN] = 0; /* Return each byte, or zero for timeout. */
+    raw.c_cc[VTIME] = 1; /* 100 ms timeout (unit is tens of second). */
+
+    /* put terminal in raw mode after flushing */
+    if (tcsetattr(fd, TCSAFLUSH, &raw) < 0)
+        return -1;
+        
+    kilo.E.rawmode = 1;
+    // no error, set to false so that scope(exit)
+    // dont modify errno
+    err = false;
+    return 0;
+}
+
 /* Load the specified program in the editor memory and returns 0 on success
  * or 1 on error. */
-int editorOpen(char *filename) {
-    cio.FILE *fp;
+int editorOpen(char* filename)
+{
+    cio.FILE* fp;
 
     kilo.E.dirty = 0;
     free(kilo.E.filename);
-    size_t fnlen = strlen(filename)+1;
+    size_t fnlen = strlen(filename) + 1;
     kilo.E.filename = cast(char*) malloc(fnlen);
-    memcpy(kilo.E.filename,filename,fnlen);
+    memcpy(kilo.E.filename, filename, fnlen);
 
-    fp = cio.fopen(filename,"r");
-    if (!fp) {
-        if (errno != ENOENT) {
+    fp = cio.fopen(filename, "r");
+    if (!fp)
+    {
+        if (errno != ENOENT)
+        {
             cio.perror("Opening file");
             exit(1);
         }
         return 1;
     }
 
-    char *line = null;
+    char* line = null;
     size_t linecap = 0;
     cbuiltin.ssize_t linelen;
     // cast away the shared in fp.
-    while((linelen = getline(&line,&linecap, cast(cbuiltin._IO_FILE*) fp)) != -1) {
-        if (linelen && (line[linelen-1] == '\n' || line[linelen-1] == '\r'))
+    while ((linelen = getline(&line, &linecap, cast(cbuiltin._IO_FILE*) fp)) != -1)
+    {
+        if (linelen && (line[linelen - 1] == '\n' || line[linelen - 1] == '\r'))
             line[--linelen] = '\0';
-        editorInsertRow(kilo.E.numrows,line,linelen);
+        editorInsertRow(kilo.E.numrows, line, linelen);
     }
     free(line);
     fclose(fp);
@@ -97,10 +150,7 @@ void initEditor()
     kilo.E.filename = null;
     kilo.E.syntax = null;
     updateWindowSize();
-    // dmd complains about the function being type void.
-    extern (C) void function(int) han = &handleSigWinCh;
-    // added 0 arg, dmd is more strict.
-    signal(SIGWINCH, han);
+    signal(SIGWINCH, &handleSigWinCh);
 }
 
 int main(string[] args)
