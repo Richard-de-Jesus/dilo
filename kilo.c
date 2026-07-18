8d15f6c6812615f1f783ec4ddc6731515f0239ea
nerd184 <135471576+nerd184@users.noreply.github.com>
2026-07-18 12:07:30 -0300

    translated EditorConfig struct
    renamed E struct to ED, to not conflict with the E constant in std.math

--- kilo.c
+++ kilo.c
@@ -78,21 +78,3 @@ typedef struct erow {
 typedef struct hlcolor {
     int r,g,b;
 } hlcolor;
-
-struct editorConfig {
-    int cx,cy;  /* Cursor x and y position in characters */
-    int rowoff;     /* Offset of row displayed. */
-    int coloff;     /* Offset of column displayed. */
-    int screenrows; /* Number of rows that we can show */
-    int screencols; /* Number of cols that we can show */
-    int numrows;    /* Number of rows */
-    int rawmode;    /* Is terminal raw mode enabled? */
-    erow *row;      /* Rows */
-    int dirty;      /* File modified but not saved. */
-    char *filename; /* Currently open filename */
-    char statusmsg[80];
-    time_t statusmsg_time;
-    struct editorSyntax *syntax;    /* Current syntax highlight, or NULL. */
-};
-
-static struct editorConfig E;
